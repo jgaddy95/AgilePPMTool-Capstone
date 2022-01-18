1a5a7427d1bf9a11f4429e8d86a2f4d26fced189
Justin Gaddy <justingaddy1@gmail.com>
2022-01-17 23:55:58 -0500

    Find All Projects

--- PPMToolFullStack/ppmtool/src/main/java/io/hcl/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/hcl/ppmtool/web/ProjectController.java
@@ -38,4 +38,7 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project,HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
 }
