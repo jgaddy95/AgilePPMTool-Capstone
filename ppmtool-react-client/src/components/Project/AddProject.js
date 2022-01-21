2345864a948ffb298dfd640a6a111fc6c11abd58
Justin Gaddy <justingaddy1@gmail.com>
2022-01-20 23:37:39 -0500

    Get validation errors from Redux

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -13,12 +13,20 @@ class AddProject extends Component {
             "projectIdentifier": "",
             "description": "",
             "start_date": "",
-            "end_date": "" 
+            "end_date": "" ,
+            "errors": {}
         }
 
         this.onChange = this.onChange.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
 
+    }
+
+    //life cyle hooks
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            this.setState({errors:nextProps.errors})
+        }
     }
 
     onChange(e) {
@@ -39,8 +47,14 @@ class AddProject extends Component {
     }
 
   render() {
+      const {errors} = this.state
+      
     return (
+
+        <div>
+        
             <div className="project">
+            
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 m-auto">
@@ -56,6 +70,7 @@ class AddProject extends Component {
                                     value={this.state.projectName}
                                     onChange={this.onChange}
                                     />
+                                    <p>{errors.projectName}</p>
                             </div>
                             <div className="form-group">
                                 <input 
@@ -66,6 +81,7 @@ class AddProject extends Component {
                                     value={this.state.projectIdentifier}
                                     onChange={this.onChange}
                                     />
+                                    <p>{errors.projectIdentifier}</p>
                             </div>
                         
                             <div className="form-group">
@@ -76,6 +92,7 @@ class AddProject extends Component {
                                     value={this.state.description}
                                     onChange={this.onChange}
                                     ></textarea>
+                                    <p>{errors.description}</p>
                             </div>
                             <h6>Start Date</h6>
                             <div className="form-group">
@@ -104,18 +121,25 @@ class AddProject extends Component {
                 </div>
             </div>
         </div>
-
+     </div>
 
      );
   }
 }
 
+
+
 AddProject.propTypes = {
-    createProject: PropTypes.func.isRequired
+    createProject: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 }
+
+const mapStateToProps = state => ({
+    errors: state.errors
+})
 
 
 export default  connect(
-    null, 
+    mapStateToProps, 
     {createProject}
     )(AddProject);
