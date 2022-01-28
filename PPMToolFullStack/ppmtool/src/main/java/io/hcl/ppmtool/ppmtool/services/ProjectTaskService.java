cdbe72cf824c6a4cf66a8dbfaf7d2959e147e98b
Justin Gaddy <justingaddy1@gmail.com>
2022-01-27 19:56:45 -0500

    Persist Project Task (Bug fix pending: setPriority)

---
+++ PPMToolFullStack/ppmtool/src/main/java/io/hcl/ppmtool/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,48 @@
+package io.hcl.ppmtool.ppmtool.services;
+
+import io.hcl.ppmtool.ppmtool.domain.Backlog;
+import io.hcl.ppmtool.ppmtool.domain.ProjectTask;
+import io.hcl.ppmtool.ppmtool.repositories.BacklogRepository;
+import io.hcl.ppmtool.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+
+        //Pts to be added to a specific project, project !=null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set the backlog to the project task
+        projectTask.setBacklog(backlog);
+
+        // we want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        //Update the backlog sequence
+        BacklogSequence++;
+
+        //Add sequence to project task
+        projectTask.setProjectSequence(backlog.getProjectIdentifier()+ "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //INITIAL priority when priority null
+//        if(projectTask.getPriority()==0 || projectTask.getPriority()==null){
+//            projectTask.setPriority(3);
+//        }
+
+        if(projectTask.getStatus()=="" || projectTask.getStatus()==null){
+            projectTask.setStatus("TO_DO");
+        }
+        //initial is null
+        return projectTaskRepository.save(projectTask);
+
+    }
+}
