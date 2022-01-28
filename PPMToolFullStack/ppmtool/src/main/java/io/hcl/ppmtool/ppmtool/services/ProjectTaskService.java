d5676f3119b02eb76b80013ffa75bc15caf72e57
Justin Gaddy <justingaddy1@gmail.com>
2022-01-28 09:11:52 -0500

    ***BUG FIX*** delete operation, improved backlog/project task rel

--- PPMToolFullStack/ppmtool/src/main/java/io/hcl/ppmtool/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/hcl/ppmtool/ppmtool/services/ProjectTaskService.java
@@ -10,8 +10,6 @@ import io.hcl.ppmtool.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ProjectTaskService {
 
@@ -105,12 +103,7 @@ public class ProjectTaskService {
     }
 
     public void deletePTByProjectSequence(String backlog_id,String pt_id){
-
         ProjectTask projectTask = findPTByProjectSequence(backlog_id,pt_id);
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = projectTask.getBacklog().getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
 
         projectTaskRepository.delete(projectTask);
     }
