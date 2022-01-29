4f100e2c8f99a5bf9ca60226992646f02db7b697
Justin Gaddy <justingaddy1@gmail.com>
2022-01-29 14:38:13 -0500

    Types and Reducers for Project Tasks

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -2,3 +2,8 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+//
+export const GET_BACKLOG = "DELETE_PROJECT";
+export const GET_PROJECT_TASK = "DELETE_PROJECT";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT";
